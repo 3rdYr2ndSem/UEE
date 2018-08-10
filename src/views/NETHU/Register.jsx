eff17af81d04226284625d77455c8375e76f1f14
BookNetwork <bookfun388@gmail.com>
2018-08-10 10:49:54 +0530

    update registration part from fazlan

--- src/views/NETHU/Register.jsx
+++ src/views/NETHU/Register.jsx
@@ -9,13 +9,30 @@ export default class Register extends Component{
                         <div className="modal-content">
                         <div className="modal-header">
                             <button type="button" className="close" data-dismiss="modal">&times;</button>
-                            <h4 className="modal-title">Registeration</h4>
+                            <h4 className="modal-title">Registeration 123</h4>
                         </div>
 
                         <div className="modal-body">
-                            <p>Some text in the modal.</p>
+                        
+                            <form >
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputEmail1">Email address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputPassword1">Password</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                </div>
+                                <div className="form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                    <label class="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
                         </div>
-                        <div className="modal-footer">
+
+                        <div className="modal-footer ">
                             <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
                         </div>
                         </div>
